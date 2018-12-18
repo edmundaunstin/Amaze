dafc5a27cd29204e224a8bbdb4c234d4a35147b1
edmundaunstin <ea130997@gmail.com>
2018-12-18 20:38:22 +0530

    First

---
+++ Assets/scene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Example());
+	}
+	
+	IEnumerator Example()
+	{
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+}
